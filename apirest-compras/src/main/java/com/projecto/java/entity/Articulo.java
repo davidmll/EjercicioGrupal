44ccfb1a79c68472dd8f4f3c38add543b9cb5cd4
gonzalezFernandez <andres.gonzalez-fernandez@capgemini.com>
2022-03-02 13:40:37 +0100

    art

--- apirest-compras/src/main/java/com/projecto/java/entity/Articulo.java
+++ apirest-compras/src/main/java/com/projecto/java/entity/Articulo.java
@@ -2,7 +2,7 @@ package com.projecto.java.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
