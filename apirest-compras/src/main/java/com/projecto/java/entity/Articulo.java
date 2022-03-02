9c8a961a5014113f2f88ed7778d83069bb6e12cd
gonzalezFernandez <andres.gonzalez-fernandez@capgemini.com>
2022-03-02 13:39:44 +0100

    articulo6

--- apirest-compras/src/main/java/com/projecto/java/entity/Articulo.java
+++ apirest-compras/src/main/java/com/projecto/java/entity/Articulo.java
@@ -1,5 +1,7 @@
 package com.projecto.java.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,7 +13,10 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="articulos")
-public class Articulo {
+public class Articulo implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
