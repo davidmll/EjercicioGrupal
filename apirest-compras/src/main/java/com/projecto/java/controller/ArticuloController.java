c80619686b571279a95c59f805abd427be0648b3
gonzalezFernandez <andres.gonzalez-fernandez@capgemini.com>
2022-03-02 18:03:49 +0100

    Merge branch 'master' of https://github.com/davidmll/EjercicioGrupal.git

--- apirest-compras/src/main/java/com/projecto/java/controller/ArticuloController.java
+++ apirest-compras/src/main/java/com/projecto/java/controller/ArticuloController.java
@@ -10,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,7 +53,71 @@ public class ArticuloController {
 		return new ResponseEntity<Articulo>(articulo, HttpStatus.OK);
 	}
 	
+	@PostMapping("/articulo")
+	public ResponseEntity<?> savePorducto(@RequestBody Articulo articulo){
+		Articulo articuloNew = null;
+		
+		Map<String, Object>response = new HashMap<>();
+		
+		try {
+			articuloNew = servicio.save(articulo);
+			
+			
+		}catch (DataAccessException e) {//MUY ESPECÍFICO, EXCEPCIONES SOBRE EL DAO
+			response.put("mensaje", "Error al rellenar la consulta a base de datos");
+			response.put("error", e.getMessage().concat(e.getMostSpecificCause().getMessage()));
+			
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El articulo ha sido creado con éxito!");
+		response.put("producto", articuloNew);
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
 	
+	@PutMapping("/articulo/{id}")
+	public ResponseEntity<?> updateCliente(@RequestBody Articulo articulo, @PathVariable Long codArticulo) {
+		
+		Articulo articuloActual = servicio.findById(codArticulo);
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		if(articuloActual == null) {
+			response.put("mensaje", "Error: no se pudo editar. El producto con Codigo: "+codArticulo.toString()+" no existe en la base de datos");
+			return new ResponseEntity<Map<String,Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		try {
+		articuloActual.setCodArticulo(codArticulo);
+		articuloActual.setUnidades_stock(articulo.getUnidades_stock());
+		articuloActual.setStock_seguridad(articulo.getStock_seguridad());
+		articuloActual.setImagen(articulo.getImagen());
+		articuloActual.setNombre(articulo.getNombre());
+		articuloActual.setPrecio_unidad(articulo.getPrecio_unidad());
+		articuloActual.setDescripcion(articulo.getDescripcion());
+		
+		servicio.save(articuloActual);
+		
+		}catch (DataAccessException e) {//MUY ESPECÍFICO, EXCEPCIONES SOBRE EL DAO
+			response.put("mensaje", "Error al rellenar la consulta a base de datos");
+			response.put("error", e.getMessage().concat(e.getMostSpecificCause().getMessage()));
+			
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El articulo ha sido actualizado con éxito!");
+		response.put("cliente", articuloActual);
+		
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+		
+		
+	}
+	
+//	@DeleteMapping("/articulo/{id}")
+//	public void deleteArticulo(@PathVariable Long codArticulo) {
+//		servicio.delete(codArticulo);
+//	}
 	
 	
 }
