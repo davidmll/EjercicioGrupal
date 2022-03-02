5407791bafa95455e111978f5bb0a0dd4ffb1ea2
Patricia <patricia.jerez-prado@capgemini.com>
2022-03-02 13:51:03 +0100

    Rematada entidad cliente

--- apirest-compras/src/main/java/com/projecto/java/entity/Cliente.java
+++ apirest-compras/src/main/java/com/projecto/java/entity/Cliente.java
@@ -9,21 +9,21 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="clientes")
-public class Cliente implements Serializable{
-	
-	
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
-	@Id 
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long codCliente;
-	
+
 	private String nombre;
 	private String apellidos;
 	private String empresa;
@@ -31,76 +31,96 @@ public class Cliente implements Serializable{
 	private int cp;
 	private String provincia;
 	private int telefono;
-	
+
 	@Temporal(TemporalType.DATE)
-	private Date  fechaNacimiento;	
-	
+	private Date fechaNacimiento;
+
 	@OneToMany(mappedBy = "cliente")
-    private Set<Compras> compras;
-	
-	
-	
+	private Set<Compras> compras;
+
+	@PrePersist
+	public void prePersist() {
+		fechaNacimiento = new Date();
+	}
+
 	public Long getCodCliente() {
 		return codCliente;
 	}
+
 	public void setCodCliente(Long codCliente) {
 		this.codCliente = codCliente;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellidos() {
 		return apellidos;
 	}
+
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+
 	public String getEmpresa() {
 		return empresa;
 	}
+
 	public void setEmpresa(String empresa) {
 		this.empresa = empresa;
 	}
+
 	public String getPuesto() {
 		return puesto;
 	}
+
 	public void setPuesto(String puesto) {
 		this.puesto = puesto;
 	}
+
 	public int getCp() {
 		return cp;
 	}
+
 	public void setCp(int cp) {
 		this.cp = cp;
 	}
+
 	public String getProvincia() {
 		return provincia;
 	}
+
 	public void setProvincia(String provincia) {
 		this.provincia = provincia;
 	}
+
 	public int getTelefono() {
 		return telefono;
 	}
+
 	public void setTelefono(int telefono) {
 		this.telefono = telefono;
 	}
+
 	public Date getFechaNacimiento() {
 		return fechaNacimiento;
 	}
+
 	public void setFechaNacimiento(Date fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
-	public Set<Compra> getCompras() {
+
+	public Set<Compras> getCompras() {
 		return compras;
 	}
-	public void setCompras(Set<Compra> compras) {
+
+	public void setCompras(Set<Compras> compras) {
 		this.compras = compras;
 	}
-	
-	
 
 }
